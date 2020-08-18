aa823f693cc8976b9110967a2c2903adfb77a338
Aloisio Vieira <aloisio.vieira@gmail.com>
2020-08-17 21:38:28 -0300

    v40 change

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World V30";
+		return "Hello World V40";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
